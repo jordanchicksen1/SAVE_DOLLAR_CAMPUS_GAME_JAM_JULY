ffcb92252137917a5ae75cb9901c3b3fc1d93719
profoundBoi <2651374@students.wits.ac.za>
2024-07-12 16:25:13 +0200

    connor stuff

--- SAVE_DOLLAR_CAMPUS_GAME_JAM_MINI/Assets/Scipts/RobotAi.cs
+++ SAVE_DOLLAR_CAMPUS_GAME_JAM_MINI/Assets/Scipts/RobotAi.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class RobotAi : MonoBehaviour
@@ -7,19 +8,77 @@ public class RobotAi : MonoBehaviour
     public Transform player;
     public float speed = 3.0f;
     public float stoppingDistance = 2.0f;
-    public int damageAmount = 10;  // Amount of damage to deal
-   //ublic playerHealth playerHealth1;
-    public CharacterController characterController;
+   // public int damageAmount = 10;
+    //public playerHealth playerHealth1;
+   // public CharacterController characterController;
+
+    private Rigidbody2D rb;
+  
+    //public GameObject spriteUp;
+    //public GameObject spriteDown;
+   // public GameObject spriteLeft;
+    //public GameObject spriteRight;
+
+   // public bool isActive = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
         float distanceToPlayer = Vector2.Distance(transform.position, player.position);
-        if (distanceToPlayer <stoppingDistance)
+        if (distanceToPlayer < stoppingDistance)
         {
             Vector2 direction = (player.position - transform.position).normalized;
             transform.Translate(direction * speed * Time.deltaTime, Space.World);
         }
+
+       // if (rb.velocity.x > 0)
+       // {
+        //    // Moving right
+         //   spriteRight.SetActive(true);
+         //   spriteLeft.SetActive(false);
+        //    spriteUp.SetActive(false);
+      //      spriteDown.SetActive(false);
+       // }
+       /// else if (rb.velocity.x < 0)
+       // {
+            // Moving left
+          //  spriteRight.SetActive(false);
+         //   spriteLeft.SetActive(true);
+       //     spriteUp.SetActive(false);
+        //    spriteDown.SetActive(false);
+      //  }
+
+       // if (rb.velocity.y > 0)
+       // {
+            // Moving up
+         //   spriteRight.SetActive(false);
+        //    spriteLeft.SetActive(false);
+        //    spriteUp.SetActive(true);
+      //      spriteDown.SetActive(false);
+      //  }
+        //else if (rb.velocity.y < 0)
+       // {
+        //    // Moving down
+         //   spriteRight.SetActive(false);
+         //   spriteLeft.SetActive(false);
+         //   spriteUp.SetActive(false);
+        //    spriteDown.SetActive(true);
+       // }
+
+        Debug.Log(rb.velocity.x);
+        Debug.Log(speed);
     }
+}
+
+   
 
  
-}
+
+
+
+
+
--- SAVE_DOLLAR_CAMPUS_GAME_JAM_MINI/Assets/Scipts/Zone.cs
+++ SAVE_DOLLAR_CAMPUS_GAME_JAM_MINI/Assets/Scipts/Zone.cs
@@ -48,4 +48,5 @@ public class Zone : MonoBehaviour
 
         transform.localScale = temp;
     }
+      
 }
