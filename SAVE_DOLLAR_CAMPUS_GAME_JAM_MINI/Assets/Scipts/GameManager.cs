612c701b909ec576dc80b1e360e4c1034d5eafa5
profoundBoi <2651374@students.wits.ac.za>
2024-07-13 10:48:51 +0200

    gameManager

---
+++ SAVE_DOLLAR_CAMPUS_GAME_JAM_MINI/Assets/Scipts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject pausePanel;
+    public void pauseGame()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+    }
+
+    public void resumeGame()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+    public void startGame()
+    {
+
+    }
+  
+}
