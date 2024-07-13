b9b59e915843e52fc22e1cd5ff22201d44ad0761
profoundBoi <2651374@students.wits.ac.za>
2024-07-13 12:54:01 +0200

    paused button added

--- SAVE_DOLLAR_CAMPUS_GAME_JAM_MINI/Assets/Scipts/gameManager.cs
+++ SAVE_DOLLAR_CAMPUS_GAME_JAM_MINI/Assets/Scipts/gameManager.cs
@@ -6,16 +6,19 @@ using UnityEngine.SceneManagement;
 public class gameManager : MonoBehaviour
 {
     public GameObject pausePanel;
+    public GameObject pauseButton;
     public void pauseGame()
     {
         Time.timeScale = 0;
         pausePanel.SetActive(true);
+        pauseButton.SetActive(false);
     }
 
     public void resumeGame()
     {
         Time.timeScale = 1;
         pausePanel.SetActive(false);
+        pauseButton.SetActive(true);
     }
 
     public void exitGame()
