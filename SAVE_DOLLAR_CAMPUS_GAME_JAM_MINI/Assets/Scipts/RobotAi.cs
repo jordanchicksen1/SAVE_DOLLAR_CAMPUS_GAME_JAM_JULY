af8c73b2379b95fe8296d8beaad8f8b562102243
profoundBoi <2651374@students.wits.ac.za>
2024-07-15 16:13:56 +0200

    Added new bot script

--- SAVE_DOLLAR_CAMPUS_GAME_JAM_MINI/Assets/Scipts/RobotAi.cs
+++ SAVE_DOLLAR_CAMPUS_GAME_JAM_MINI/Assets/Scipts/RobotAi.cs
@@ -7,11 +7,17 @@ public class RobotAi : MonoBehaviour
     public Transform[] player, player2;
     public float speed = 3.0f;
     public float stoppingDistance = 2.0f;
+    [SerializeField] float destroyDelay = 0.1f;
     // public int damageAmount = 10;
     //public playerHealth playerHealth1;
     // public CharacterController characterController;
 
     private Rigidbody2D rb;
+
+    public GameObject explosion;
+
+    SpriteRenderer sr;
+    RobotAi Ai;
 
     
     private void Start()
@@ -36,9 +42,19 @@ public class RobotAi : MonoBehaviour
             Vector2 direction = (player2[0].position - transform.position).normalized;
             transform.Translate(direction * speed * Time.deltaTime, Space.World);
         }
-
        
+    }
 
-       
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Bot detroyed");
+            Destroy(rb.gameObject, destroyDelay);
+            //Bot.SetActive(false);
+            explosion.SetActive(true);
+            sr.sprite = null;
+            Ai.Ai = null;
+        }
     }
 }
